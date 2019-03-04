c1652a91e778af17c0c77a0b8f51651acd558e13
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-04 09:51:06 -0500

    finished MonthConversion

--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,15 +1,24 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Use a map to solve
  */
 public class MonthConversion {
+    private Map<Integer, String> months;
+
+    public MonthConversion() {
+        months = new TreeMap<>();
+    }
+
     /**
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
     public void add(Integer monthNumber, String monthName) {
-
+        months.put(monthNumber, monthName);
     }
 
     /**
@@ -17,15 +26,20 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+        return months.get(monthNumber);
     }
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
-    public int getNumber(String monthName) {
-        return (Integer)null;
+    public Integer getNumber(String monthName) {
+        for (Integer key : months.keySet()) {
+            if (months.get(key).equals(monthName)) {
+                return key;
+            }
+        }
+        return null;
     }
 
     /**
@@ -33,7 +47,11 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        if (getName(monthNumber) == null){
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
@@ -41,14 +59,18 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        if (getNumber(monthName) == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return months.size();
     }
 
     /**
@@ -56,6 +78,6 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+        months.replace(monthNumber, monthName);
     }
 }
