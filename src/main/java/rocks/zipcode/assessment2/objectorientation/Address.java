33aee5057377403d13ad42aac451c33e6669b272
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-04 11:00:32 -0500

    Address.java

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Address.java
@@ -1,10 +1,16 @@
 package rocks.zipcode.assessment2.objectorientation;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * @author leon on 28/11/2018.
  */
 public class Address {
+    private Map<String, String> address;
+
     public Address() {
+        this("", "", "", "", "");
     }
 
     /**
@@ -15,45 +21,60 @@ public class Address {
      * @param zipcode - zipcode of region
      */
     public Address(String addressLine1, String addressLine2, String city, String state, String zipcode) {
+        address = new TreeMap<>();
+        address.put("addressLine1", addressLine1);
+        address.put("addressLine2", addressLine2);
+        address.put("city", city);
+        address.put("state", state);
+        address.put("zipcode", zipcode);
     }
 
     public String getAddressLine1() {
-        return null;
+        return address.get("addressLine1");
     }
 
     public void setAddressLine1(String addressLine1) {
+        address.put("addressLine1", addressLine1);
     }
 
     public String getAddressLine2() {
-        return null;
+        return address.get("addressLine2");
     }
 
     public void setAddressLine2(String addressLine2) {
+        address.put("addressLine2", addressLine2);
     }
 
     public String getCity() {
-        return null;
+        return address.get("city");
     }
 
     public void setCity(String city) {
+        address.put("city", city);
     }
 
     public String getState() {
-        return null;
+        return address.get("state");
     }
 
     public void setState(String state) {
+        address.put("state", state);
     }
 
     public String getZipcode() {
-        return null;
+        return address.get("zipcode");
     }
 
     public void setZipcode(String zipcode) {
+        address.put("zipcode", zipcode);
+    }
+
+    public boolean equals(Address a) {
+        return address.equals(a.address);
     }
 
     @Override
-    public boolean equals(Object o) {
-        return (Boolean)null;
+    public String toString() {
+        return "Address" + address.toString().replaceAll("=", "='").replaceAll(",", "',").replaceAll("[}]", "'}");
     }
 }
